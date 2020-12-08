1896cd4adb47d57ce8db49c33fe99add94c9be39
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-12-08 01:26:35 -0300

    Logging

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { UserRepository } from './user.repository';
@@ -7,6 +7,8 @@ import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
+    private logger: Logger = new Logger('AuthService');
+
     constructor(
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
@@ -28,6 +30,9 @@ export class AuthService {
         }
         const payload: JwtPayload = { username };
         const accessToken = this.jwtService.sign(payload);
+        this.logger.debug(
+            `Generated JWT with payload ${JSON.stringify(payload)}`,
+        );
 
         return { accessToken };
     }
