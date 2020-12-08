176dfd1b688af6c9b9407cdc4c0a7d7ffc3f21af
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-12-08 16:01:20 -0300

    Config for production

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-    const serverConfig: { port: number } = config.get('server');
+    const serverConfig: any = config.get('server');
     const logger = new Logger('bootstrap');
     const app = await NestFactory.create(AppModule);
 
-    if (process.env.NODE_ENV === 'development') app.enableCors();
+    if (process.env.NODE_ENV === 'development') {
+        app.enableCors();
+    } else {
+        app.enableCors({ origin: serverConfig.origin });
+    }
 
     const port = process.env.PORT || serverConfig.port;
     await app.listen(port);
